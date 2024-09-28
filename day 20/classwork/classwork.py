a6c6e4f3ee5e7d19db667b7a28e5445811a5c2ad
sakhvadzemate1 <matesakhvadze176@gmail.com>
2024-09-28 20:51:37 +0400

    Add files via upload

---
+++ day 20/classwork/classwork.py
@@ -0,0 +1,26 @@
+#for i in range (3) :
+    #print('hello world')
+
+
+
+
+
+
+#lives = 10
+#while lives >0:
+    ##lives = lives - 1
+
+
+
+#age=0
+#while age <=10
+# print('hello world')
+    #age = age + 1
+
+
+
+
+for i in range(3):
+   print( i < 2)
+#!= es nishnavs anu ar udris 
+
